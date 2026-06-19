fc6812a87da9628acac0a13603bb13dee1eedc34
Vedika kolap <vedikakolap07@gmail.com>
2026-06-19 20:34:11 +0530

    render url hardcode

--- task-frontend/src/App.jsx
+++ task-frontend/src/App.jsx
@@ -12,7 +12,7 @@ import {
 } from "lucide-react";
 import "./App.css";
 
-const API_URL = import.meta.env.VITE_API_URL;
+const API_URL = 'https://task-manager-fullstack-06lu.onrender.com';
 
 const PRIORITY_CONFIG = {
   LOW: { label: "Low", className: "badge-low" },
